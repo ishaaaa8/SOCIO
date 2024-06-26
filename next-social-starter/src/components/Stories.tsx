e6e2b791c75625703354c59fbcd99210c6c65d92
ishaaaa8 <itsisha4884@gmail.com>
2024-06-26 22:16:27 +0530

    stories

--- next-social-starter/src/components/Stories.tsx
+++ next-social-starter/src/components/Stories.tsx
@@ -2,15 +2,47 @@ import React from 'react'
 import Image from 'next/image'
 function Stories() {
   return (
-    <div className='p-4 bg-white shadow-md overflow-scroll text-xs'>
-      <div className='flex gap-8'>
+    <div className='p-4 bg-white shadow-md overflow-scroll text-xs scrollbar-hide'>
+      <div className='flex gap-8 w-max'>
 
             <div className='flex flex-col items-center gap-2 cursor-pointer'>
 
-            <Image src="" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+            <Image src="https://images.pexels.com/photos/26699663/pexels-photo-26699663/free-photo-of-a-woman-walking-down-a-path-in-the-woods.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
             <span className='font-medium'>Isha</span>
             </div>
+
+            <div className='flex flex-col items-center gap-2 cursor-pointer'>
+
+<Image src="https://images.pexels.com/photos/26699663/pexels-photo-26699663/free-photo-of-a-woman-walking-down-a-path-in-the-woods.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+<span className='font-medium'>Isha</span>
+</div>
+<div className='flex flex-col items-center gap-2 cursor-pointer'>
+
+<Image src="https://images.pexels.com/photos/26699663/pexels-photo-26699663/free-photo-of-a-woman-walking-down-a-path-in-the-woods.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+<span className='font-medium'>Isha</span>
+</div>
+<div className='flex flex-col items-center gap-2 cursor-pointer'>
+
+<Image src="https://images.pexels.com/photos/26699663/pexels-photo-26699663/free-photo-of-a-woman-walking-down-a-path-in-the-woods.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+<span className='font-medium'>Isha</span>
+</div>
+<div className='flex flex-col items-center gap-2 cursor-pointer'>
+
+<Image src="https://images.pexels.com/photos/26699663/pexels-photo-26699663/free-photo-of-a-woman-walking-down-a-path-in-the-woods.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+<span className='font-medium'>Isha</span>
+</div>
+<div className='flex flex-col items-center gap-2 cursor-pointer'>
+
+<Image src="https://images.pexels.com/photos/26699663/pexels-photo-26699663/free-photo-of-a-woman-walking-down-a-path-in-the-woods.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+<span className='font-medium'>Isha</span>
+</div>
+<div className='flex flex-col items-center gap-2 cursor-pointer'>
+
+<Image src="https://images.pexels.com/photos/26699663/pexels-photo-26699663/free-photo-of-a-woman-walking-down-a-path-in-the-woods.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2"/>
+<span className='font-medium'>Isha</span>
+</div>
       </div>
+
     </div>
   )
 }
